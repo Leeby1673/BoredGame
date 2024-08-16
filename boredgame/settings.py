c8feef10fc25568814e5746dfdd99a344ad1af7b
陳宏威 <ert60213@gmail.com>
2024-08-16 19:33:56 +0800

    add GCP setting and install gunicorn

--- boredgame/settings.py
+++ boredgame/settings.py
@@ -86,21 +86,37 @@ WSGI_APPLICATION = "boredgame.wsgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
-
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.mysql",
-        "NAME": "boredgame",
-        "USER": "root",
-        "PASSWORD": "greed9527",
-        "HOST": "127.0.0.1",
-        "PORT": "3306",
-        "OPTIONS": {
-            "charset": "utf8mb4",
-            "init_command": "SET sql_mode='STRICT_TRANS_TABLES'",
-        },
+if os.getenv("GAE_APPLICATION", None):
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.mysql",
+            "NAME": os.getenv("DB_NAME"),
+            "USER": os.getenv("DB_USER"),
+            "PASSWORD": os.getenv("DB_PASSWORD"),
+            "HOST": os.getenv("DB_HOST"),
+            "PORT": "3306",
+            "OPTIONS": {
+                "charset": "utf8mb4",
+                "init_command": "SET sql_mode='STRICT_TRANS_TABLES'",
+            },
+        }
     }
-}
+# 若沒有 "GAE_APPLICATION" 變數，則使用以下連接到下方資料庫
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.mysql",
+            "NAME": "boredgame",
+            "USER": "root",
+            "PASSWORD": "greed9527",
+            "HOST": "127.0.0.1",
+            "PORT": "3306",
+            "OPTIONS": {
+                "charset": "utf8mb4",
+                "init_command": "SET sql_mode='STRICT_TRANS_TABLES'",
+            },
+        }
+    }
 
 
 # Password validation
